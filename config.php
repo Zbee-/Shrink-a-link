7551c6fd26d97b30e7483ada493e31722a566b81
Ethan Henderson <ethan@zbee.me>
2014-05-26 14:13:00 -0600

    ~Don't worry about it

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-    $db_connect   =    mysql_connect("localhost", "qafbuevv_zbee", "VO46kQSfLc7w") or die(mysql_error());
+    $db_connect   = mysql_connect("localhost", "**DATABASE USERNAME**", "**DATABASE PASSWORD**") or die(mysql_error());
       //Database Connection
       //Default: "mysql_connect("localhost", "**DATABASE USERNAME**", "**DATABASE PASSWORD**") or die(mysql_error())"
 
-    $db_select    =    mysql_select_db("qafbuevv_zbee");
+    $db_select    = mysql_select_db("**DATABASE NAME**");
       //Database Selection
       //Default: "mysql_select_db("**DATABASE NAME**")"
       
@@ -15,7 +15,7 @@
       //The root url of your installment of S-a-L (No trailing slash)
       //Default: "http://**YOUR SITE**.com"
       
-    $char_string  =   "abcdefghijklmnopqrstuvwxyz0123456789";
+    $char_string  = "abcdefghijklmnopqrstuvwxyz0123456789";
       //The selection of characters that a shrink comes from (note: S-a-L does not currently recognize capitalization)
       //Default: "abcdefghijklmnopqrstuvwxyz0123456789"
 
